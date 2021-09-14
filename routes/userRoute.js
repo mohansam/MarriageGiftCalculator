27a7aa53152125a6f43a42a6c00be94989be5bea
mohansam <52488729+mohansam@users.noreply.github.com>
2021-09-14 15:37:57 +0530

    router added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,3 +6,5 @@ router.get("/users", userController.get_user);
 router.post("/signup", userController.signup_user);
 router.post("/login", userController.login_user);
 router.post("/delete/:userId", userController.delete_user);
+
+module.exports = router;
