511005991082fa08c490fd2c7e0826bdeed622e5
mohansam <52488729+mohansam@users.noreply.github.com>
2021-09-14 19:05:43 +0530

    Process.env varaible created

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
-const DATABASE_URL =
-  "mongodb+srv://Mohan:1234@marriagecalculator.u1ox4.mongodb.net/UserDatabase?retryWrites=true&w=majority";
+//const DATABASE_URL =
+//"mongodb+srv://Mohan:1234@marriagecalculator.u1ox4.mongodb.net/UserDatabase?retryWrites=true&w=majority";
 
-mongoose.connect(DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
