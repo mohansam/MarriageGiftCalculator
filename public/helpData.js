d2e57fcc5376aaf8258122685bfd3ea96d08be7c
mohansam <52488729+mohansam@users.noreply.github.com>
2021-09-18 16:58:33 +0530

    Complted REST API

--- public/helpData.js
+++ public/helpData.js
@@ -12,15 +12,21 @@ module.exports.helpData = [
         "Signup end point": [
           "method-Post",
           "body-[UserName,UserEmail,UserPwd]",
+          "res-[UserName,UserEmail]",
         ],
       },
       {
-        "login end point": ["method-Post", "body-[UserEmail,UserPwd]"],
+        "login end point": [
+          "method-Post",
+          "body-[UserEmail,UserPwd]",
+          "res-[UserName,UserEmail]",
+        ],
       },
       {
         "delete end point": [
           "method-Delete",
           "body-[]",
+          "res-[return count of deletd id]",
           "Cookie-jwt(user id is populated from Cookie)",
         ],
       },
@@ -34,12 +40,15 @@ module.exports.helpData = [
           "/api/attendee/createattendee",
           "/api/attendee/updateattendee",
           "/api/attendee/deleteattendee",
+          "/api/attendee/searchattendeename",
+          "/api/attendee/searchattendeecity",
         ],
       },
       {
         "getattendee end point": [
           "method-Get",
           "body-[]",
+          "res-[[AttendeeName,AttendeeCity,AttendeeAmount,_Id]]",
           "Cookie-jwt(user id is populated from Cookie)",
         ],
       },
@@ -47,6 +56,7 @@ module.exports.helpData = [
         "createattendee end point": [
           "method-Post",
           "body-[AttendeeName,AttendeeAmount,AttendeeCity]",
+          "res-[AttendeeName,AttendeeCity,AttendeeAmount,_Id]",
           "Cookie-jwt(user id is populated from Cookie)",
         ],
       },
@@ -54,6 +64,7 @@ module.exports.helpData = [
         "updateattendee end point": [
           "method-Patch",
           "body-[AttendeeName,AttendeeAmount,AttendeeCity,AttendeeId]",
+          "res-[AttendeeName,AttendeeCity,AttendeeAmount,_Id]",
           "Cookie-jwt(user id is populated from Cookie)",
         ],
       },
@@ -61,6 +72,23 @@ module.exports.helpData = [
         "deleteattendee end point": [
           "method-Patch",
           "body-[AttendeeId]",
+          "res-[return count of deletd id]",
+          "Cookie-jwt(user id is populated from Cookie)",
+        ],
+      },
+      {
+        "searchattendeename end point": [
+          "method-GET",
+          "body-[],queryParameter-AttendeeName",
+          "res-[[AttendeeName,AttendeeCity,AttendeeAmount,_Id]]",
+          "Cookie-jwt(user id is populated from Cookie)",
+        ],
+      },
+      {
+        "searchattendeecity end point": [
+          "method-GET",
+          "body-[],queryParameter-AttendeeCity",
+          "res-[[AttendeeName,AttendeeCity,AttendeeAmount,_Id]]",
           "Cookie-jwt(user id is populated from Cookie)",
         ],
       },
