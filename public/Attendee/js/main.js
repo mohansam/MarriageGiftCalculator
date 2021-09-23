f68f15679d042d196a51e966501921a76371694b
mohansam <52488729+mohansam@users.noreply.github.com>
2021-09-24 00:09:43 +0530

    update and delete completed

--- public/Attendee/js/main.js
+++ public/Attendee/js/main.js
@@ -20,12 +20,7 @@
       $(".modal:visible").each(alignModal);
     });
   });
-  var input = {
-    _id: "614aa130210a1f0004d06578",
-    AttendeeName: "navin",
-    AttendeeAmount: 100,
-    AttendeeCity: "Mula",
-  };
+
   function renderTableRow(input, rownumber) {
     var tBody = document.querySelector("#tBody");
     var r1 = tBody.insertRow(rownumber);
@@ -48,7 +43,12 @@
       deleteUserPopulateTable(event.target);
     });
   }
-
+  function updateTableRow(input) {
+    var row = document.getElementById(input._id);
+    row.children[0].innerText = input.AttendeeName;
+    row.children[1].innerText = input.AttendeeAmount;
+    row.children[2].innerText = input.AttendeeCity;
+  }
   async function getAttendee() {
     try {
       const URI = window.origin + "/api/v1/attendee/getattendee";
@@ -73,21 +73,117 @@
   }
 
   function editUserPopulateTable(ele) {
+    if ($(ele).hasClass("edit")) {
+      ele = ele.children[0];
+    }
     var row = ele.parentElement.parentElement.parentElement;
-    document.querySelector(
-      "#editEmployeeModal > div > div > form > div:nth-child(1) > input"
-    ).value = row.children[0].innerText;
-    document.querySelector(
-      "#editEmployeeModal > div > div > form > div:nth-child(2) > input"
-    ).value = row.children[1].innerText;
-    document.querySelector(
-      "#editEmployeeModal > div > div > form > div:nth-child(3) > input"
-    ).value = row.children[2].innerText;
+    var id = "_" + row.attributes.id.value;
+    var updateForm = document.getElementById("updateForm").children[0];
+    updateForm.setAttribute("id", id);
+    document.getElementById("updateUserName").value = row.children[0].innerText;
+    document.getElementById("updateUserAmount").value =
+      row.children[1].innerText;
+    document.getElementById("updateUserCity").value = row.children[2].innerText;
   }
+
   function deleteUserPopulateTable(ele) {
     var row = ele.parentElement.parentElement.parentElement;
-    document.querySelector(
-      "#deleteEmployeeModal > div > div > div:nth-child(1) > h4"
-    ).innerText = `Delete ${row.children[0].innerText}`;
+    var id = "_" + row.attributes.id.value;
+    var deleteElment = document.getElementById("hdeleteUser").children[0];
+    deleteElment.setAttribute("id", id);
+    deleteElment.innerText = `Delete ${row.children[0].innerText}`;
+  }
+
+  $("#deleteButton").on("click", () => {
+    $("#deleteButton").attr("disabled", true);
+    $("#deleteModelCloseButton").attr("disabled", true);
+    var deleteElment = document.getElementById("hdeleteUser").children[0];
+    var id = deleteElment.attributes.id.value.substr(1);
+    deleteAttendee(id);
+  });
+
+  $("#deleteModelCloseButton").on("click", () => {
+    $("#deleteButton").attr("disabled", false);
+    document.getElementById("deleteAlert").innerText = "";
+  });
+
+  async function deleteAttendee(id) {
+    try {
+      $("#deleteLoader").addClass("lds-ellipsis");
+      const URI = window.origin + "/api/v1/attendee/deleteattendee";
+      const res = await fetch(URI, {
+        method: "DELETE",
+        body: JSON.stringify({ AttendeeId: id }),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.error) {
+        $("#deleteLoader").removeClass("lds-ellipsis");
+        $("#deleteModelCloseButton").attr("disabled", false);
+      } else {
+        document.getElementById("deleteAlert").innerText = "Deleted!";
+        document.getElementById(id).remove();
+        console.log(data);
+        $("#deleteLoader").removeClass("lds-ellipsis");
+        $("#deleteModelCloseButton").attr("disabled", false);
+      }
+    } catch (err) {
+      $("#deleteLoader").removeClass("lds-ellipsis");
+      $("#deleteModelCloseButton").attr("disabled", false);
+      console.log("from catch");
+      console.log(err);
+    }
+  }
+
+  $("#updateButton").on("click", () => {
+    $("#updateButton").attr("disabled", true);
+    $("#updateModelCloseButton").attr("disabled", true);
+    updateAttendee();
+  });
+
+  $("#updateModelCloseButton").on("click", () => {
+    $("#updateButton").attr("disabled", false);
+    document.getElementById("updateAlert").innerText = "";
+  });
+
+  async function updateAttendee() {
+    try {
+      $("#updateLoader").addClass("lds-ellipsis");
+      const URI = window.origin + "/api/v1/attendee/updateattendee";
+      const AttendeeName = document.getElementById("updateUserName").value;
+      const AttendeeAmount = document.getElementById("updateUserAmount").value;
+      const AttendeeCity = document.getElementById("updateUserCity").value;
+      var updateForm = document.getElementById("updateForm").children[0];
+      const AttendeeId = updateForm.attributes.id.value.substr(1);
+      const res = await fetch(URI, {
+        method: "PATCH",
+        body: JSON.stringify({
+          AttendeeName,
+          AttendeeAmount,
+          AttendeeCity,
+          AttendeeId,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+      if (data.error) {
+        console.log("errordata");
+        $("#updateLoader").removeClass("lds-ellipsis");
+        $("#updateModelCloseButton").attr("disabled", false);
+        $("#updateButton").attr("disabled", false);
+      } else {
+        updateTableRow(data);
+        $("#updateLoader").removeClass("lds-ellipsis");
+        $("#updateModelCloseButton").attr("disabled", false);
+        $("#updateButton").attr("disabled", false);
+        document.getElementById("updateAlert").innerText = "Updated!";
+      }
+    } catch (err) {
+      console.log("from catch");
+      console.log(err);
+      $("#updateLoader").removeClass("lds-ellipsis");
+      $("#updateModelCloseButton").attr("disabled", false);
+    }
   }
 })(jQuery);
